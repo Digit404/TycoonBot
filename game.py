508cacf63f0f012c264317b9a6270e78745fecd4
Daniel Gonzales <daniel.gonzales.26@us.af.mil>
2022-11-01 14:04:27 -0500

    added some initial stuff

--- game.py
+++ game.py
@@ -1,12 +1,30 @@
 #! python
+import pygame # pygame FTW
+import numpy # for complex maths
 
-import pygame
-
+#constants
 WIDTH = 1920
 HEIGHT = 1080
 
+#setup
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 pygame.display.set_caption("March")
 
+tick = 0
+
+# THE LOOP
 while True:
-    pass
+    # Event handling 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+    
+    tick += 1
+
+    pygame.display.flip()
