d2017768a633603c68e0031ac053de8f69686c13
Daniel Gonzales <daniel.gonzales.26@us.af.mil>
2022-11-01 13:39:48 -0500

    initial commit

---
+++ game.py
@@ -0,0 +1,12 @@
+#! python
+
+import pygame
+
+WIDTH = 1920
+HEIGHT = 1080
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+pygame.display.set_caption("March")
+
+while True:
+    pass
